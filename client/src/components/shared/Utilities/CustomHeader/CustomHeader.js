a062a5178ff57d8aa71b7e451572dc03c352c161
unknown <abedalnabi96@gmail.com>
2025-04-15 17:08:07 +0300

    day2 #4

--- client/src/components/shared/Utilities/CustomHeader/CustomHeader.js
+++ client/src/components/shared/Utilities/CustomHeader/CustomHeader.js
@@ -3,35 +3,36 @@ import { Box, Typography, Breadcrumbs, Link, Container } from '@mui/material';
 
 const ShopLeftSidebar = ({ title, breadcrumbLinks }) => {
 	return (
-		<Container>
-			<Box
-				sx={{
-					padding: '20px',
-					backgroundColor: '#f5f5f5',
-					height: '200px',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'center',
-				}}
-				textAlign={'left'}
-			>
-				<Typography variant="h4" fontWeight="bold" color="primary">
-					{title}
-				</Typography>
+		<Box
+			sx={{
+				padding: '20px',
+				backgroundColor: '#f6f5ff',
+				height: '200px',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+			}}
+		>
+			<Container sx={{ padding: '20px' }}>
+				<Box textAlign={'left'}>
+					<Typography variant="h4" fontWeight="bold">
+						{title}
+					</Typography>
 
-				<Breadcrumbs aria-label="breadcrumb" sx={{ marginTop: '10px' }}>
-					{breadcrumbLinks.map((link, index) => (
-						<Link
-							key={index}
-							color={index === breadcrumbLinks.length - 1 ? 'text.primary' : 'inherit'}
-							href={link.href}
-						>
-							{link.label}
-						</Link>
-					))}
-				</Breadcrumbs>
-			</Box>
-		</Container>
+					<Breadcrumbs aria-label="breadcrumb" sx={{ marginTop: '10px' }}>
+						{breadcrumbLinks?.map((link, index) => (
+							<Link
+								key={index}
+								color={index === breadcrumbLinks.length - 1 ? 'text.primary' : 'inherit'}
+								href={link.href}
+							>
+								{link.label}
+							</Link>
+						))}
+					</Breadcrumbs>
+				</Box>
+			</Container>
+		</Box>
 	);
 };
 
