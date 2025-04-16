337ec4fdd5efa4819b2d25a32a503ce12be2bef2
unknown <abedalnabi96@gmail.com>
2025-04-16 21:27:22 +0300

    day3 #13

---
+++ client/src/components/shared/complete/section/staticText.js
@@ -0,0 +1,8 @@
+const STATIC_TEXT = {
+	TITLE: 'Your Order Is Completed!',
+	DESCRIPTION:
+		'Thank you for your order! Your order is being processed and will be completed within 3-6 hours. You will receive an email confirmation when your order is completed.',
+	BUTTON: 'Continue Shopping',
+};
+
+export default STATIC_TEXT;
---
+++ client/src/components/shared/complete/section/style.js
@@ -0,0 +1,31 @@
+const styles = {
+	container: {
+		textAlign: 'center',
+		mt: 5,
+		width: '650px',
+	},
+	iconBox: {
+		mt: 4,
+	},
+	icon: {
+		fontSize: 60,
+		color: 'green',
+	},
+	title: {
+		mt: 2,
+		fontWeight: 'bold',
+	},
+	subtitle: {
+		mt: 2,
+		color: 'gray',
+	},
+	button: {
+		mt: 4,
+		backgroundColor: '#FF4081',
+		'&:hover': {
+			backgroundColor: '#D500F9',
+		},
+	},
+};
+
+export default styles;
